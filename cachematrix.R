719f79d7080c22d79828e8688d83d741d941e0cc
Andrushka <andrey.tyshchenko@gmail.com>
2015-09-24 12:32:57 -0500

    Final Submission
    Working Script.

--- cachematrix.R
+++ cachematrix.R
@@ -1,32 +1,32 @@
+################# ASSIGNMENT_2 ###################################################
 ## makeCacheMatrix() - creates a special "matrix" object that can cache its inverse
 ## cacheSolve() - function computes the inverse of the special "matrix" returned by makeCacheMatrix above
+###################################################################################
 
-
-
+## MakeCacheMatrix() an object that consists of 2 parts: set & get. 
+## NOTE: in addition to $set you can also assign matrix by doing "a <- makeCacheMatrix(y)"
 makeCacheMatrix <- function(x = matrix()) {
-  inv <- NULL
-  ## set matrix()
+  inv <<- NULL
   set <- function(y) {
     x <<- y
     inv <<- NULL
   }
-  ## get matrix()
   get <- function() x
-  ##cacheSolve()
-
-  cacheSolve <- function(x, ...) {
-      ## if 
-      if ( !is.null(inv) ) {
-        message("###Inverted matrix has already been calculated### \r\n ###THE RESULT IS:")
-        return(inv)
-      }
-      ## Return a matrix that is the inverse of 'x'
-      data <- x$get()
-      inv <<- solve(data)
-      inv
-  }
-  list(set = set, get = get,
-       cacheSolve = cacheSolve)
+  list(set = set, get = get)
 }
+
+##cacheSolve(x) checks inv variable is empty, if not, returns cached value. 
+cacheSolve <- function(x, ...) {
+  ## if 
+  if ( !is.null(inv) ) {
+    message("###Inverted matrix has already been calculated### \r\n ###THE RESULT IS:")
+    return(inv)
+  }
+  ## Return a matrix that is the inverse of 'x'
+  data <- x$get()
+  inv <<- solve(data)
+  inv
+}
+
 
 
